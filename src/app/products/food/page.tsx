47f4156279f56504cd53b515d547cd4a07b74f56
alhiefikri <alhiepedia@gmail.com>
2025-02-21 23:54:27 +0800

    feat: navigation with: Link, router, redirect

---
+++ src/app/products/food/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+  return (
+    <div className="h-[900px]">
+      <h2>Food Page</h2>
+      <button
+        onClick={() => router.push("/products")}
+        className="border-4 border-slate-700 text-green-400"
+      >
+        Go Back to products
+      </button>
+    </div>
+  );
+}
--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,14 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
   return (
     <>
-      <ul className="flex gap-4">
-        <li>Fashion</li>
-        <li>Sports</li>
-        <li>Technology</li>
+      <ul className="flex text-sm text-blue-300 gap-4">
+        <Link
+          className={pathname === "/products/food" ? "text-red-200" : ""}
+          scroll={false}
+          href="/products/food"
+        >
+          Food
+        </Link>
+        <Link
+          className={pathname === "/products/sports" ? "text-red-200" : ""}
+          replace={true}
+          href="/products/sports"
+        >
+          Sports
+        </Link>
+        <Link
+          className={pathname === "/products/technology" ? "text-red-200" : ""}
+          href="/products/technology"
+        >
+          Technology
+        </Link>
       </ul>
       {children}
     </>
