4cda4d1b9c9b9d440b76122675accbdde90d0dfe
alhiefikri <alhiepedia@gmail.com>
2025-02-22 22:31:27 +0800

    feat: learn method POST, GET section

---
+++ src/app/posts/data.ts
@@ -0,0 +1,20 @@
+export const posts = [
+  {
+    id: 1,
+    text: "This is post 1",
+    userId: 3,
+    description: "lorem ipsum",
+  },
+  {
+    id: 3,
+    text: "This is post 3",
+    userId: 5,
+    description: "lorem ipsum",
+  },
+  {
+    id: 3,
+    text: "This is post 3",
+    userId: 7,
+    description: "lorem ipsum",
+  },
+];
