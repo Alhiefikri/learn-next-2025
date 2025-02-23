bbd74d9b5a1d53b77302e500c18dc3e2d1e11788
alhiefikri <alhiepedia@gmail.com>
2025-02-23 17:47:26 +0800

    feat: learn handler Cookies section

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,13 +1,20 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   // const headersList = new Headers(request.headers);
   const headerList = headers();
   console.log(headerList.get("Authorization"));
+  console.log(request.cookies.get("token"));
+
+  cookies().set("theme", "dark");
+
+  console.log(cookies().get("theme"));
+
   return new Response("<h1>Profile Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "token=test123",
     },
   });
 }
